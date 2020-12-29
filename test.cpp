517cff40650252697ca3452651b66bf729d02091
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2020-12-29 23:48:07 +0530

    Updated

--- test.cpp
+++ test.cpp
@@ -1,49 +1,23 @@
 // contest code
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 int main(){
-    int t,p,f;
-    int ans = 1;
-    cin>>t;
-    while(t>0){
-        cin>>p>>f;
-        int low = 1,high = f,mid;
-        mid = (high + low)/2;
-        while(low<high){
-            ans++;
-            low = mid + 1;
-            mid = (high + low)/2;
+    string textInput;
+    int count = 0;
+    getline(cin,textInput);
+    // scanf("%[^\n]s",textInput);
+    for (int i = 0; i < textInput.length(); i++){
+        if((textInput[i] >= 'a' && textInput[i] <= 'z')||(textInput[i] >= 'A' && textInput[i] <= 'Z')||(textInput[i] >= '0' && textInput[i] <= '9')||textInput[i] == ' '){
+            continue;
         }
-        cout<<ans;
-        t--;
-    }
+        else{
+            count++;
+        }
+    }    
+    cout << count;
 }   
 
 
 
-
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-
-int main() {
-    int t,p,f;
-    cin>>t;
-    while(t>0){
-        cin>>p>>f;
-        int ans;
-        ans = log2(f) + 1;
-        cout<<ans;
-        t--;
-    }
-    return 0;
-}
