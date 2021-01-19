7832b35a2017d321068d55499042f47bfcda6715
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2021-01-19 21:59:41 +0530

    Minor Changes

--- MansaAndStones.cpp
+++ MansaAndStones.cpp
@@ -11,12 +11,12 @@ void printArray(vector<int> a) {
 
 vector<int> stones(int n, int a, int b) {
     vector<int> last;
-    for(int i=0,j=n-1;i<n&&j>=0;i++,j--){   
-        int item=a*i+b*j;   //finding a,b multiples with i&j
+    for(int i = 0, j = n - 1; i < n && j >= 0; i++, j--){   
+        int item = a * i + b * j;   //finding a,b multiples with i&j
         last.push_back(item);          
     }
     
-    sort(last.begin(),last.end()); //Sorting  
+    sort(last.begin(), last.end()); //Sorting  
     auto it = unique(last.begin(), last.end()); //Removing Duplicates
     last.resize(distance(last.begin(), it));
     return last;
