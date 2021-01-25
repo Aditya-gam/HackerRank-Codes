060415e48aa85b4633d435b0e76f90f5f78aea4f
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2021-01-25 10:21:24 +0530

    Create 2_Operators.cpp

---
+++ 30 Days Of Code/2_Operators.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the solve function below.
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+    float tip = (float)meal_cost * tip_percent / 100;
+    float tax = (float)meal_cost * tax_percent / 100;
+    meal_cost += tip + tax;
+    cout << round(meal_cost);
+}
+
+int main()
+{
+    double meal_cost;
+    cin >> meal_cost;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');    //This line ignores the rest of the current line, up to '\n' or EOF - whichever comes first:
+
+    int tip_percent;
+    cin >> tip_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');    //This line ignores the rest of the current line, up to '\n' or EOF - whichever comes first
+
+    int tax_percent;
+    cin >> tax_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');    //This line ignores the rest of the current line, up to '\n' or EOF - whichever comes first
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
