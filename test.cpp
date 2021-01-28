a538c6f3b1e4d749dc7061a181c2dd5d9f1e5793
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2021-01-28 17:20:27 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,36 +4,42 @@
 using namespace std;
 
 int main(){
-    string textInput;
-    int count = 0;
-    getline(cin,textInput);
-    // scanf("%[^\n]s",textInput);
-    for (int i = 0; i < textInput.length(); i++){
-        if((textInput[i] >= 'a' && textInput[i] <= 'z')||(textInput[i] >= 'A' && textInput[i] <= 'Z')||(textInput[i] >= '0' && textInput[i] <= '9')||textInput[i] == ' '){
-            continue;
-        }
-        else{
-            count++;
-        }
-    }    
-    cout << count;
+
+    long double pa = 38.87204214882674, pb = 77.05630496209294, ka = 5575212980, kb = 3761741224;
+
+    cout << endl << pa * pb << endl;
+
+    // cout << endl << ka * kb << endl;
+    // string textInput;
+    // int count = 0;
+    // getline(cin,textInput);
+    // // scanf("%[^\n]s",textInput);
+    // for (int i = 0; i < textInput.length(); i++){
+    //     if((textInput[i] >= 'a' && textInput[i] <= 'z')||(textInput[i] >= 'A' && textInput[i] <= 'Z')||(textInput[i] >= '0' && textInput[i] <= '9')||textInput[i] == ' '){
+    //         continue;
+    //     }
+    //     else{
+    //         count++;
+    //     }
+    // }    
+    // cout << count;
 }   
 
 
 
-struct Player {
-    string name;
-    int score;
-}; 
-bool comp(Player p1, Player p2){
-    if(p1.score>p2.score) return true;
-    if(p1.score==p2.score) return p1.name<p2.name;
-    return false;
-}
-vector<Player> comparator(vector<Player> players) {
-    sort(players.begin(),players.end(), comp);
-    return players;
-}
+// struct Player {Mm
+//     string name;
+//     int score;
+// }; 
+// bool comp(Player p1, Player p2){
+//     if(p1.score>p2.score) return true;
+//     if(p1.score==p2.score) return p1.name<p2.name;
+//     return false;
+// }
+// vector<Player> comparator(vector<Player> players) {
+//     sort(players.begin(),players.end(), comp);
+//     return players;
+// }
 
 // if(a.score > b.score) {
 //             return 1;
